3fa922f2876aaeeb85bb5c69a5ad05ed3fd960a4
meganletendre <megan.letendre@comcast.net>
2018-07-13 13:42:59 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,26 @@
-#write out your code here
-
 def least_coins(cents)
+  if cents >= 25
+    num_quarters = cents/25 
+    cents = cents - 25*num_quarters
+  else num_quarters = 0 
+  end
+  if cents >= 10 
+    num_dimes = cents/10
+    cents = cents - 10*num_dimes
+  else num_dimes = 0 
+  end
+  if cents >= 5
+    num_nickels = cents/5
+    cents = cents - 5*num_nickels
+  else num_nickels = 0 
+  end 
+  if cents >= 1
+    num_pennies = cents/1 
+    cents = cents - num_pennies
+  else num_pennies = 0 
+  end
+  total_coins = { :quarters => num_quarters, :dimes => num_dimes, :nickels => num_nickels, :pennies => num_pennies}
+end 
 
-#Code your answer here!
 
-end
+puts least_coins(465)
